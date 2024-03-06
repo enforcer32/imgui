ed32a0a068a32f5c0af136b62c397e42d90aaf02
enforcer <160966530+enforcer32@users.noreply.github.com>
2024-03-05 19:50:55 -0500

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,51 @@
+project "imgui"
+kind "StaticLib"
+language "C++"
+cppdialect "C++17"
+staticruntime "off"
+
+targetdir ("Build/" .. outputdir .. "/%{prj.name}")
+objdir ("Build/Intermediates/" .. outputdir .. "/%{prj.name}")
+
+files
+{
+    "imconfig.h",
+    "imgui.h",
+    "imgui.cpp",
+    "imgui_draw.cpp",
+    "imgui_internal.h",
+    "imgui_widgets.cpp",
+    "imstb_rectpack.h",
+    "imstb_textedit.h",
+    "imstb_truetype.h",
+    "imgui_demo.cpp",
+    "imgui_tables.cpp",
+    "backends/imgui_impl_glfw.h",
+    "backends/imgui_impl_glfw.cpp",
+    "backends/imgui_impl_opengl3_loader.h",
+    "backends/imgui_impl_opengl3.h",
+    "backends/imgui_impl_opengl3.cpp",
+}
+
+includedirs
+{
+    ".",
+    "%{IncludeDir.glfw}",
+}
+
+links
+{
+    "glfw",
+    "opengl32.lib",
+}
+
+filter "system:windows"
+    systemversion "latest"
+
+filter "configurations:Debug"
+    runtime "Debug"
+    symbols "on"
+
+filter "configurations:Release"
+    runtime "Release"
+    optimize "on"
